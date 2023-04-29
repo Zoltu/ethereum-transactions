87e6eb39bf7ab918d7c42493ef4fb6d184cdc2fb
Micah Zoltu <micah@zoltu.net>
2023-04-30 02:01:59 +0800

    Adds functions for getting transaction hash, signer address, and address from private key.
    Address from private key may be out of scope of this repository, but I'm going to leave it in for now singe we need most of the same code to get the transaction signer address.

--- source/tests/transaction.ts
+++ source/tests/transaction.ts
@@ -1,7 +1,7 @@
 import { describe, should } from 'micro-should'
 import { testCases } from './ethers-transactions.js'
-import { Transaction, Transaction1559Unsigned, Transaction155Unsigned, Transaction2930Unsigned, TransactionLegacyUnsigned, decodeTransaction, encodeTransaction, signTransaction } from '../transaction.js'
-import { addressHexToBigint, hexToBigint, hexToBytes } from '../converters.js'
+import { Transaction, Transaction1559Unsigned, Transaction155Unsigned, Transaction2930Unsigned, TransactionLegacyUnsigned, decodeTransaction, encodeTransaction, getSigner, isSigned, signTransaction } from '../transaction.js'
+import { addressHexToBigint, bigintToHex, hexToBigint, hexToBytes } from '../converters.js'
 import { assertEqual } from './utils.js'
 
 async function test(unsigned: Transaction, privateKey: bigint, unsignedRlp: Uint8Array, signedRlp: Uint8Array) {
@@ -96,6 +96,25 @@ describe('1559', () => {
 				accessList: (testCase.transaction.accessList ?? []).map(tuple => [addressHexToBigint(tuple.address), tuple.storageKeys.map(hexToBigint)])
 			}
 			await test(unsigned, hexToBigint(testCase.privateKey), hexToBytes(testCase.unsignedLondon), hexToBytes(testCase.signedLondon))
+		})
+	}
+})
+
+describe('recover sender', () => {
+	const cases = [
+		{ name: 'legacy1', signerAddress: 0xd4fe407789e11a27b7888a324ec597435353dc35n, signedTransactionBytes: hexToBytes('0xf86b018502540be40082520894df90dea0e0bf5ca6d2a7f0cb86874ba6714f463e872386f26fc100008029a0c933e91702f2131f5565eafbad1eaccb6086dab9c5d6eba1bf454f3106099d3da015467094f7d60d0cd16702cee4460863cb94165714dd0d26a0df24995f785dcf') },
+		{ name: 'legacy2', signerAddress: 0x801040E2965D0cf9d73FEe4ccc8eEA9eeBbC491en, signedTransactionBytes: hexToBytes('0xf89982028882237e819d946eb893e3466931517a04a17d153a6330c3f2f1dd82c854b5889e365e59664fb881554ba1175519b5195b1d20390beb806d8f2cda7893e6f79848195dba4c905db6d7257ffb5eefea35f18ae33c1ba0ba61344dc955b2f0e5dbc3c65e023e1c718539465131acb8a51b2ef75620114aa03366e9f2294bf2eca7322f3954b9b38745c40602239e3d7fa693667206907518') },
+		{ name: '155', signerAddress: 0x801040E2965D0cf9d73FEe4ccc8eEA9eeBbC491en, signedTransactionBytes: hexToBytes('0xf89e82028882237e819d946eb893e3466931517a04a17d153a6330c3f2f1dd82c854b5889e365e59664fb881554ba1175519b5195b1d20390beb806d8f2cda7893e6f79848195dba4c905db6d7257ffb5eefea35f18ae33c850108097e62a0f141fe1b7e2fc1ed5d2b6ea4f04f92053e18f07274e2bda1c6852438c1895229a075553a7ae158a3fd46f75b547e847b59e2876f9a42de7a26d016db33232516de') },
+		{ name: '2930', signerAddress: 0x801040E2965D0cf9d73FEe4ccc8eEA9eeBbC491en, signedTransactionBytes: hexToBytes('0x01f89f848404bf1f82028882237e819d946eb893e3466931517a04a17d153a6330c3f2f1dd82c854b5889e365e59664fb881554ba1175519b5195b1d20390beb806d8f2cda7893e6f79848195dba4c905db6d7257ffb5eefea35f18ae33cc080a0775f29642af1045b40e5beae8e6bce2dc9e222023b7a50372be6824dbb7434fba05dacfff85752a0b9fd860bc751c17235a670d318a8b9494d664c1b87e33ac8dd') },
+		{ name: '1559', signerAddress: 0x801040E2965D0cf9d73FEe4ccc8eEA9eeBbC491en, signedTransactionBytes: hexToBytes('0x02f8a5848404bf1f820288832c7e6384346d9246819d946eb893e3466931517a04a17d153a6330c3f2f1dd82c854b5889e365e59664fb881554ba1175519b5195b1d20390beb806d8f2cda7893e6f79848195dba4c905db6d7257ffb5eefea35f18ae33cc080a0f1003f96c6c6620dd46db36d2ae9f12d363947eb0db088c678b6ad1cf494aa6fa06085b5abbf448de5d622dc820da590cfdb6bb77b41c6650962b998a941f8d701') },
+		// { name: '', signerAddress: 0x0n, signedTransactionBytes: hexToBytes('') },
+	]
+	for (const testCase of cases) {
+		should(testCase.name, () => {
+			const transaction = decodeTransaction(testCase.signedTransactionBytes)
+			if (!isSigned(transaction)) throw new Error(`wat?`)
+			const actualSigner = getSigner(transaction)
+			assertEqual(bigintToHex(testCase.signerAddress), bigintToHex(actualSigner))
 		})
 	}
 })
--- source/transaction.ts
+++ source/transaction.ts
@@ -1,9 +1,11 @@
-import { bigintToBytes } from './converters.js'
-import { JsonTransaction155, Transaction155, Transaction155Signed, decodeTransaction155, deserializeTransaction155, encodeTransaction155, isEncodedTransaction155, isSigned155, serializeTransaction155, sign155 } from './transaction-155.js'
-import { JsonTransaction1559, Transaction1559, Transaction1559Signed, decodeTransaction1559, deserializeTransaction1559, encodeTransaction1559, isSigned1559, serializeTransaction1559, sign1559 } from './transaction-1559.js'
-import { JsonTransaction2930, Transaction2930, Transaction2930Signed, decodeTransaction2930, deserializeTransaction2930, encodeTransaction2930, isSigned2930, serializeTransaction2930, sign2930 } from './transaction-2930.js'
-import { JsonTransactionLegacy, TransactionLegacy, TransactionLegacySigned, decodeTransactionLegacy, deserializeTransactionLegacy, encodeTransactionLegacy, isSignedLegacy, serializeTransactionLegacy, signLegacy } from './transaction-legacy.js'
+import { Signature, getPublicKey } from '@noble/secp256k1'
+import { bigintToBytes, bytesToBigint } from './converters.js'
+import { JsonTransaction155, Transaction155, Transaction155Signed, Transaction155Unsigned, decodeTransaction155, deserializeTransaction155, encodeTransaction155, isEncodedTransaction155, isSigned155, serializeTransaction155, sign155 } from './transaction-155.js'
+import { JsonTransaction1559, Transaction1559, Transaction1559Signed, Transaction1559Unsigned, decodeTransaction1559, deserializeTransaction1559, encodeTransaction1559, isSigned1559, serializeTransaction1559, sign1559 } from './transaction-1559.js'
+import { JsonTransaction2930, Transaction2930, Transaction2930Signed, Transaction2930Unsigned, decodeTransaction2930, deserializeTransaction2930, encodeTransaction2930, isSigned2930, serializeTransaction2930, sign2930 } from './transaction-2930.js'
+import { JsonTransactionLegacy, TransactionLegacy, TransactionLegacySigned, TransactionLegacyUnsigned, decodeTransactionLegacy, deserializeTransactionLegacy, encodeTransactionLegacy, isSignedLegacy, serializeTransactionLegacy, signLegacy } from './transaction-legacy.js'
 import { DistributedOmit, assertNever } from './typescript.js'
+import { keccak_256 } from '@noble/hashes/sha3'
 
 export * from './transaction-legacy.js'
 export * from './transaction-155.js'
@@ -11,6 +13,7 @@ export * from './transaction-2930.js'
 export * from './transaction-1559.js'
 
 export type TransactionSigned = TransactionLegacySigned | Transaction155Signed | Transaction2930Signed | Transaction1559Signed
+export type TransactionUnsigned = TransactionLegacyUnsigned | Transaction155Unsigned | Transaction2930Unsigned | Transaction1559Unsigned
 export type Transaction = TransactionLegacy | Transaction155 | Transaction2930 | Transaction1559
 export type JsonTransaction = JsonTransactionLegacy | JsonTransaction155 | JsonTransaction2930 | JsonTransaction1559
 
@@ -70,6 +73,36 @@ export function isSigned(transaction: Transaction): transaction is TransactionSi
 		case '2930': return isSigned2930(transaction)
 		case '1559': return isSigned1559(transaction)
 	}
+}
+
+function publicKeyToAddress(uncompressedPublicKey: Uint8Array): bigint {
+	const addressBytes = keccak_256(uncompressedPublicKey.subarray(1, 65)).slice(12)
+	return bytesToBigint(addressBytes)
+}
+
+export function getAddress(privateKey: bigint): bigint {
+	const publicKey = getPublicKey(privateKey, false)
+	return publicKeyToAddress(publicKey)
+}
+
+export function getTransactionHash(transaction: Transaction): bigint {
+	const encoded = encodeTransaction(transaction)
+	return bytesToBigint(keccak_256(encoded))
+}
+
+export function getSigner(transaction: TransactionSigned): bigint {
+	const recovery = Number('yParity' in transaction ? transaction.yParity : transaction.v % 2n ? 0n : 1n)
+	const signature = new Signature(transaction.r, transaction.s, recovery)
+	const unsigned: TransactionUnsigned = { ...transaction }
+	if ('yParity' in unsigned) delete unsigned.yParity
+	if ('v' in unsigned) delete unsigned.v
+	if ('r' in unsigned) delete unsigned.r
+	if ('s' in unsigned) delete unsigned.s
+	const unsignedRlp = encodeTransaction(unsigned)
+	const unsignedHash = keccak_256(unsignedRlp)
+	const publicKey = signature.recoverPublicKey(unsignedHash)
+	const encodedPublicKey = publicKey.toRawBytes(false)
+	return publicKeyToAddress(encodedPublicKey)
 }
 
 export function signTransaction(transaction: TransactionLegacy, privateKey: bigint): Promise<TransactionLegacySigned>
